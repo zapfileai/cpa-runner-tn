65b1d29eec929b313c0437cd358becacbb0fcb89
Sreehari Annam <sannam@wescodist.com>
2026-06-30 07:19:21 -0400

    feat: load first 80 cities from cities.csv

--- cities.js
+++ cities.js
@@ -1,5 +1,20 @@
-﻿const tennesseeCities = [
-  // paste first 80 cities from cities.csv here
+const tennesseeCities = [
+  "Adams", "Adamsville", "Afton", "Alamo", "Alcoa",
+  "Alexandria", "Antioch", "Apison", "Ardmore", "Arlington",
+  "Arrington", "Ashland City", "Athens", "Atoka", "Bath Springs",
+  "Baxter", "Beechgrove", "Bell Buckle", "Bells", "Belvidere",
+  "Benton", "Bethel Springs", "Big Sandy", "Birchwood", "Blaine",
+  "Bloomington Springs", "Blountville", "Bluff City", "Bolivar", "Bon Aqua",
+  "Braden", "Bradford", "Brentwood", "Brighton", "Bristol",
+  "Brownsville", "Brush Creek", "Buchanan", "Buffalo Valley", "Burlison",
+  "Burns", "Butler", "Calhoun", "Camden", "Carthage",
+  "Cedar Grove", "Cedar Hill", "Celina", "Centerville", "Chapel Hill",
+  "Chapmansboro", "Charleston", "Charlotte", "Chattanooga", "Christiana",
+  "Chuckey", "Church Hill", "Clarksville", "Cleveland", "Clinton",
+  "College Grove", "Collegedale", "Collierville", "Columbia", "Cookeville",
+  "Copperhill", "Cordova", "Corryton", "Cosby", "Cottage Grove",
+  "Cottontown", "Covington", "Cross Plains", "Crossville", "Culleoka",
+  "Cumberland City", "Cumberland Furnace", "Cunningham", "Dandridge", "Dayton",
 ];
 
 const BATCH_SIZE = 5;
